f05aa1e0b5db46b0ff0e4bdebaf4d18960154818
ericziethen <48635802+ericziethen@users.noreply.github.com>
2019-08-28 12:35:58 +1000

    Basic Python & Django Project Setup (#14)

---
+++ dev/Linting/RunPylint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MODULE_NAME=legocollector
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJ_MAIN_DIR=$SCRIPT_PATH/../..
+MODULE_PATH=$PROJ_MAIN_DIR/$MODULE_NAME
+
+echo SCRIPT_PATH: $SCRIPT_PATH
+echo PROJ_MAIN_DIR: $PROJ_MAIN_DIR
+echo MODULE_PATH: $MODULE_PATH
+
+export PYTHONPATH=$PYTHONPATH:$MODULE_PATH
+
+pylint "$MODULE_PATH"
+return_code=$?
+
+if [[ $return_code -eq  0 ]];
+then
+    echo "*** No Issues Found"
+else
+    echo "*** Some Issues Found"
+fi
+
+echo "exit $return_code"
+exit $return_code
