aa9ecf6385fd0434173c0003102e28d18fcca4f7
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-08-28 15:07:39 +1000

    Add good test

--- legocollector/test_fail.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class TestFail(TestCase):
-
-    def test_FAIL(self):
-        self.assertTrue(False)
--- tests/test_fail.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_fail():
-    assert False
