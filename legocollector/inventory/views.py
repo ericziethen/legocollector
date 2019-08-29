da01755c1d873b12d5c634bf4ea0b5efff5ecee7
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-08-30 09:33:46 +1000

    Fix linting issues

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-#from django.shortcuts import render
+# from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
+def index(request):  # pylint: disable=unused-argument
     return HttpResponse('Inventory Main Page - Placeholder')
