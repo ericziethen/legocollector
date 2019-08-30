941ca6da128ea163054d4d33cb4cd4440545f655
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-08-30 14:39:15 +1000

    misc

--- dev/Linting/RunPylint.bat
+++ dev/Linting/RunPylint.bat
@@ -5,17 +5,19 @@ setlocal
 set BATCH_DIR=%~dp0
 set PROJ_MAIN_DIR=%BATCH_DIR%..\..
 set MODULE_PATH=%PROJ_MAIN_DIR%\legocollector
+set ERROR_FOUND=
+set ERROR_LIST=
 
 set PYTHONPATH=%PYTHONPATH%;%MODULE_PATH%
 
-pylint --load-plugins pylint_django "%MODULE_PATH%"
-set return_code=%errorlevel%
-if %return_code% equ 0 (
-    echo *** No Issues Found
-    goto exit_ok
-) else (
-    echo *** Some Issues Found
+: Pylint requires __init__.py files to run, but the Django root doesn't have it so we have to specify the folders separately
+call:run_pylint "%MODULE_PATH%\legocollector"
+call:run_pylint "%MODULE_PATH%\inventory"
+
+if defined ERROR_FOUND (
     goto exit_error
+) else (
+    goto exit_ok
 )
 
 :exit_error
@@ -25,3 +27,27 @@ exit /B 1
 :exit_ok
 endlocal
 exit /B 0
+
+
+: #########################################
+: ##### START OF FUNCTION DEFINITIONS #####
+: #########################################
+:run_pylint
+set LINT_PATH=%~1
+
+echo ### PYLINT START - '%LINT_PATH%' ###
+pylint --load-plugins pylint_django "%LINT_PATH%"
+set return_code=%errorlevel%
+if %return_code% gtr 0 (
+    set ERROR_FOUND=TRUE
+    set ERROR_LIST=%ERROR_LIST% %LINT_PATH%
+    echo   Issues Found
+) else (
+    echo   No Issues
+)
+echo ### PYLINT END - '%LINT_PATH%' ###
+echo[
+goto:eof
+: #######################################
+: ##### END OF FUNCTION DEFINITIONS #####
+: #######################################
--- dev/Linting/RunPylint.sh
+++ dev/Linting/RunPylint.sh
@@ -13,7 +13,7 @@ export PYTHONPATH=$PYTHONPATH:$MODULE_PATH
 
 # Pylint has an issue finding the modules since there is not __init__.py in the django root, if it were it causes issues with django test discovery
 #pylint --load-plugins pylint_django "$MODULE_PATH"
-find . -type f -name "*.py" | xargs python -m pylint
+find . -type f -name "*.py" | xargs pylint --load-plugins pylint_django
 return_code=$?
 
 if [[ $return_code -eq  0 ]];
