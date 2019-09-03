759b6eb8d528ebf11053157ee8873858bbd18828
ericziethen <48635802+ericziethen@users.noreply.github.com>
2019-09-03 14:23:52 +1000

    Feature/basic views (#24)
    * Update Base HTML
    
    * Add Basic User Part List View
    
    * Add Logged out page
    
    * Add Nicer Name for Part
    
    * Fix Linting Issues

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import UserPart
 
 # Create your views here.
+
+
 @login_required
 def index(request):  # pylint: disable=unused-argument
     # user = request.user
     return HttpResponse('Inventory Main Page - Placeholder')
+
+
+class UserPartList(LoginRequiredMixin, ListView):  # pylint: disable=too-many-ancestors
+    model = UserPart
+
+    def get_queryset(self):
+        """Only for current user."""
+        return UserPart.objects.filter(user_id=self.request.user)
