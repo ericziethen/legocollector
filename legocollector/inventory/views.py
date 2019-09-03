71134af7991e95f3134cfe2cceaced3fb42ce2b2
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-09-03 13:43:31 +1000

    Add Basic User Part List View

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import UserPart, Color
 
 # Create your views here.
+
+# TODO - DO WE NEED DEFAULT INDEX VIEW STILL ???
 @login_required
 def index(request):  # pylint: disable=unused-argument
     # user = request.user
     return HttpResponse('Inventory Main Page - Placeholder')
+
+
+class UserPartList(LoginRequiredMixin, ListView):
+    model = UserPart
+
+    def get_queryset(self):
+        """Only for current user."""
+        return UserPart.objects.filter(user_id=self.request.user)
