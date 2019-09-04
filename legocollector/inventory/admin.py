5f096ad4227c982a52fefd89c8d062b6a4cc9b6f
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-09-04 11:17:34 +1000

    Modify some Admin views

--- legocollector/inventory/admin.py
+++ legocollector/inventory/admin.py
@@ -5,8 +5,21 @@ from .models import Part
 from .models import PartCategory
 from .models import UserPart
 
+
+class ColorAdmin(admin.ModelAdmin):
+    fields = ['name', 'transparent', 'rgb']
+
+
+class PartAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Identification', {'fields': ['part_num', 'name']}),
+        ('Dimensions', {'fields': ['width', 'height', 'length']}),
+        ('Attributes', {'fields': ['stud_count', 'multi_height', 'uneven_dimensions']}),
+    ]
+
+
 # Register your models here.
-admin.site.register(Color)
-admin.site.register(Part)
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Part, PartAdmin)
 admin.site.register(PartCategory)
 admin.site.register(UserPart)
