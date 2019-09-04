5e08abc18b8dc2d8e9eee3cc034c5a775f4a731f
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-09-05 09:03:19 +1000

    Improve home page

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from .models import UserPart
 
