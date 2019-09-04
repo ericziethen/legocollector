8870cedc27d79152d6f2b642e963a6e2d9496e8b
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-09-04 11:43:00 +1000

    Admin List displays

--- legocollector/inventory/admin.py
+++ legocollector/inventory/admin.py
@@ -8,6 +8,7 @@ from .models import UserPart
 
 class ColorAdmin(admin.ModelAdmin):
     fields = ['name', 'transparent', 'rgb']
+    list_display = ('name', 'rgb', 'transparent')
 
 
 class PartAdmin(admin.ModelAdmin):
@@ -16,10 +17,16 @@ class PartAdmin(admin.ModelAdmin):
         ('Dimensions', {'fields': ['width', 'height', 'length']}),
         ('Attributes', {'fields': ['stud_count', 'multi_height', 'uneven_dimensions']}),
     ]
+    list_display = ('part_num', 'name', 'category_id', 'width', 'height', 'length',
+                    'stud_count', 'multi_height', 'uneven_dimensions')
+
+
+class UserPartAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'part_num', 'color')
 
 
 # Register your models here.
 admin.site.register(Color, ColorAdmin)
 admin.site.register(Part, PartAdmin)
 admin.site.register(PartCategory)
-admin.site.register(UserPart)
+admin.site.register(UserPart, UserPartAdmin)
