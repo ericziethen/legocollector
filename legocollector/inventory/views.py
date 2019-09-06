851d93f7966877c26d1ca5abb6226799231375b3
eric <eric@home.com>
2019-09-06 21:46:57 +1000

    Dont allow part number when editing userpart

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -30,6 +30,29 @@ class UserPartCreateForm(ModelForm):
         return cleaned_data
 
 
+class UserPartUpdateForm(ModelForm):
+    class Meta:
+        model = UserPart
+        fields = ('color', 'qty')
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')        
+        self.part_num = kwargs.pop('part_num')
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        # Get the cleaned data
+        cleaned_data = super().clean()
+
+        # Find the unique_together fields
+        color = cleaned_data.get('color')
+
+        if UserPart.objects.filter(user_id=self.user, part_num=self.part_num, color=color).exists():
+            raise ValidationError('You already have this Userpart in your list.')
+
+        return cleaned_data
+
+
 class UserPartCreateView(LoginRequiredMixin, CreateView):  # pylint: disable=too-many-ancestors
     model = UserPart
     template_name = 'inventory/userpart_create.html'
@@ -49,11 +72,10 @@ class UserPartCreateView(LoginRequiredMixin, CreateView):  # pylint: disable=too
         return kwargs
 
 
-# TODO - Maybe Combine the 2 forms
 class UserPartUpdateView(LoginRequiredMixin, UpdateView):  # pylint: disable=too-many-ancestors
     model = UserPart
     template_name = 'inventory/userpart_update.html'
-    form_class = UserPartCreateForm
+    form_class = UserPartUpdateForm
 
     def form_valid(self, form):
         form.instance.user_id = self.request.user
@@ -65,7 +87,7 @@ class UserPartUpdateView(LoginRequiredMixin, UpdateView):  # pylint: disable=too
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs.update({'user': self.request.user})
+        kwargs.update({'user': self.request.user, 'part_num': self.object.part_num})
         return kwargs
 
 
