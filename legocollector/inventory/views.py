ff90ab6bca38b19d5b342e9960fe7178b19ac6a9
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-09-06 14:26:53 +1000

    Basic Validation Error when Creating Duplicate User parts

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import ModelForm, ValidationError
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView, DetailView, ListView, UpdateView
 from django.views.generic.edit import CreateView
@@ -6,20 +7,53 @@ from django.views.generic.edit import CreateView
 from .models import UserPart
 
 
+class UserPartCreateForm(ModelForm):
+    class Meta:
+        model = UserPart
+        fields = ('part_num', 'color', 'qty')
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        # Get the cleaned data
+        cleaned_data = super().clean()
+
+        # Find the unique_together fields
+        part_num = cleaned_data.get('part_num')
+        color = cleaned_data.get('color')
+
+        if UserPart.objects.filter(user_id=self.user, part_num=part_num, color=color).exists():
+            raise ValidationError('You already have this Userpart in your list.')
+
+        return cleaned_data
+
+
 class UserPartCreateView(LoginRequiredMixin, CreateView):  # pylint: disable=too-many-ancestors
     model = UserPart
     template_name = 'inventory/userpart_create.html'
-    fields = ['part_num', 'color', 'qty']
+    form_class = UserPartCreateForm
 
     def form_valid(self, form):
         form.instance.user_id = self.request.user
-        return super().form_valid(form)
+        try:
+            return super().form_valid(form)
+        except ValidationError:
+            form.add_error(None, 'You already have a Part for this color in your list')
+            return super().form_invalid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
 
 
 class UserPartUpdateView(LoginRequiredMixin, UpdateView):  # pylint: disable=too-many-ancestors
     model = UserPart
     template_name = 'inventory/userpart_update.html'
-    fields = ['part_num', 'color', 'qty']
+    fields = ['color', 'qty']
+    # TODO - If we allow changing color we need to check that the new piece doesn't already exist
 
 
 class UserPartDeleteView(LoginRequiredMixin, DeleteView):  # pylint: disable=too-many-ancestors
