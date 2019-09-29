5b15e6c3c1a9d8fa1e8fdafbcf50faf58a9fe104
eric <eric@home.com>
2019-09-29 12:38:28 +1000

    Fix linting issues

--- legocollector/inventory/views.py
+++ legocollector/inventory/views.py
@@ -5,7 +5,7 @@ from django.db import transaction
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms import BaseModelFormSet, ModelForm, ValidationError, modelformset_factory
+from django.forms import ModelForm, ValidationError, modelformset_factory
 from django.forms.formsets import BaseFormSet
 
 from django.http import HttpResponse, HttpResponseRedirect
@@ -360,53 +360,45 @@ class InventoryForm(ModelForm):
         fields = ['color', 'qty']
 
     def __init__(self, *args, userpart, **kwargs):
-        
-        #print('InventoryForm.__init__()')
-        '''
-        print(F'  args: {args}')
-        print(F'  kwargs: {kwargs}')
-        print(F'  Userpart: {kwargs.get("pk1")}')
-        '''
-        self.userpart = userpart
-        #print(F'  Userpart init: {userpart}')
-        #print(F'  Userpart: {self.userpart}')
-        #print(F'  Type Userpart: {type(self.userpart)}')
-        #kwargs.update({'userpart': self.kwargs.get('pk1', '')})
-        super().__init__(*args, **kwargs)
-    '''
-    def is_valid(self):
-        return super().is_valid()
-    '''
 
-    # TODO
-    # Fields and Functions for Verification and Validation
+        # print('InventoryForm.__init__()')
+        self.userpart = userpart
+        # print(F'  Userpart init: {userpart}')
+        # print(F'  Userpart: {self.userpart}')
+        # print(F'  Type Userpart: {type(self.userpart)}')
+        # kwargs.update({'userpart': self.kwargs.get('pk1', '')})
+        super().__init__(*args, **kwargs)
 
     def save(self, commit=True):
-        print('InventoryForm.save() - ENTER')
+        # print('InventoryForm.save() - ENTER')
         instance = super().save(commit=False)
         if commit:
-            print('InventoryForm.save() - commit')
+            # print('InventoryForm.save() - commit')
             instance.save()
-        print('InventoryForm.save() - EXIT')
+        # print('InventoryForm.save() - EXIT')
         return instance
 
 
-
-# TODO - We need Formset Validation to avoid Duplicate Colors
-# See https://whoisnicoleharris.com/2015/01/06/implementing-django-formsets.html
-
-#class BaseInventoryFormset(BaseFormSet):
-class BaseInventoryFormset(BaseModelFormSet):
+class BaseInventoryFormset(BaseFormSet):
 
     def __init__(self, *args, **kwargs):
-        print('BaseInventoryFormset.__init__() - ENTER')
+        # print('BaseInventoryFormset.__init__() - ENTER')
         super().__init__(*args, **kwargs)
 
-        print(F'kwargs::: {kwargs}')
-        #create filtering here whatever that suits you needs
-        self.queryset = Inventory.objects.filter(userpart=kwargs.get('userpart'))
-        print('BaseInventoryFormset.__init__() - EXIT')
+        # print(F'kwargs::: {kwargs}')
+        # create filtering here whatever that suits you needs
+        # print('userpart', kwargs.get('userpart'))
+        # print('userpart2', kwargs.get('form_kwargs').get('userpart'))
 
+        userpart = kwargs.get('form_kwargs').get('userpart')
+
+        self.queryset = Inventory.objects.filter(userpart=userpart)
+        # print('queryset', self.queryset)
+        # print('BaseInventoryFormset.__init__() - EXIT')
+
+    '''  # pylint: disable=pointless-string-statement
+    NOT WORKING YET - Probably need to check for duplicate colors
+    # See https://whoisnicoleharris.com/2015/01/06/implementing-django-formsets.html
     def clean(self):
         """
         Adds Validation that no 2 forms have the same color.
@@ -417,18 +409,23 @@ class BaseInventoryFormset(BaseModelFormSet):
         duplicates = False
 
         # Don't need to validata if invalid forms are found
+        print(F'  Check Errors::: {self.errors}')
         if any(self.errors):
+            print( '  -> Errors Found')
             return
 
         for form in self.forms:
+            print(F' Check Form:')
             # Ignore Forms that are meant for deletion
             if self.can_delete and self._should_delete_form(form):
+                print('  Ignoring form Meant for Deletion')
                 continue
 
             color = form.cleaned_data.get('color')
             if color and color in color_list:
                 duplicates = True
             color_list.append(color)
+            print(F'  Color: "{color}" - List: {color_list}')
 
             # !!! NOT WORKING YET !!!
             if duplicates:
@@ -438,6 +435,7 @@ class BaseInventoryFormset(BaseModelFormSet):
                     code='duplicate_colors'
                 )
         print('BaseInventoryFormset.clean() - EXIT')
+    '''  # pylint: disable=pointless-string-statement
 
 
 InventoryFormset = modelformset_factory(
@@ -445,143 +443,43 @@ InventoryFormset = modelformset_factory(
 )
 
 
-class UserPartManageColorsView(LoginRequiredMixin, UpdateView):
+class UserPartManageColorsView(LoginRequiredMixin, UpdateView):  # pylint: disable=too-many-ancestors
     model = UserPart
     fields = []
     template_name = 'inventory/userpart_manage_colors.html'
     pk_url_kwarg = 'pk1'
 
     def get_context_data(self, **kwargs):
-        print('UserPartManageColorsView.get_context_data() - ENTER')
         context = super().get_context_data(**kwargs)
         initial_data = [{'color': inv.color, 'qty': inv.qty}
                         for inv in Inventory.objects.filter(userpart=self.object)]
-        print(F'INITIAL DATA: {initial_data}')
-        if 'inventory_list' in context:
-            print(F'inventory_list: {context["inventory_list"]}')
-
         if self.request.POST:
-            context['inventory_list'] = InventoryFormset(self.request.POST, initial=initial_data, form_kwargs={'userpart': self.object})
+            context['inventory_list'] = InventoryFormset(self.request.POST, initial=initial_data,
+                                                         form_kwargs={'userpart': self.object})
         else:
             context['inventory_list'] = InventoryFormset(initial=initial_data, form_kwargs={'userpart': self.object})
-        #print(F'inventory_list set:::  {context["inventory_list"]}')
-        print('UserPartManageColorsView.get_context_data() - EXIT')
         return context
 
     def form_valid(self, form):
         context = self.get_context_data()
         inventory_formset = context['inventory_list']
 
-        # Verify the Forms and Formset
-        inventory_formset.full_clean()
-
-        '''
-        # Handle Any Errors
-        if any(inventory_formset.errors) or any(inventory_formset.non_form_errors()):
-            return super().form_invalid(form)
-        '''
-
-
-        #for deleted_forms in inventory_formset.deleted_forms:
-        #print(F'FORMSET: {inventory_formset}')
-        #print(F'DELETED FORMS: {inventory_formset.deleted_forms}')
-        #print(F'DELETED OBJECTS: {inventory_formset.deleted_ojects}')
-
-        '''
-        if inventory_formset.is_valid():
-            print(F'FORMSET: VALID')
-            instances = inventory_formset.save(commit=False)
-            for instance in instances:
-                # do something with instance
-                #print(F'Instance::: {instance}')
-                #print(F'    Instance.color::: {instance.color}')
-                #print(F'    Instance.userpart::: {instance.userpart}')
-                #print(F'    Instance.qty::: {instance.qty}')
-                instance.userpart = self.object
-                print(F'    New Instance.userpart::: {instance.userpart}')
-                print(F'    Instance Type::: {type(instance)}')
-                instance.save()
-        else:
-            print(F'FORMSET: INVALID')
-        '''
+        # Verify the Forms and Formset - Maybe needed??? Maybe not
+        # inventory_formset.full_clean()
 
         for inventory_form in inventory_formset:
-            #print('FORM:::', str(inventory_form))
             if inventory_form.is_valid():
-
-                print(F'HAS USERPART::: {"userpart" in inventory_form.cleaned_data}')
-                print(F'HAS id::: {"id" in inventory_form.cleaned_data}')
-                # Valid Form might be empty Forms, so check if it has all the data we need
-
-                '''
-                !!! STILL NOT UPDATING - Move saving to formset.save() !!!
-                '''
-
-                if (('color' in inventory_form.cleaned_data) and
-                    ('qty' in inventory_form.cleaned_data)):
+                # Check that it's not a blank unchanged form
+                if (('color' in inventory_form.cleaned_data) and ('qty' in inventory_form.cleaned_data)):
                     color = inventory_form.cleaned_data['color']
                     qty = inventory_form.cleaned_data['qty']
-                    #inventory = inventory_form.save(commit=False)
-                    #inventory.userpart = self.object
-                    #inventory.color = color
 
-                    # There are some issues with unique_together and form.save, so look up the the object directly
-                    inventory, _ = Inventory.objects.get_or_create(
-                        userpart=self.object,
-                        color=color
-                    )
+                    inventory, _ = Inventory.objects.get_or_create(userpart=self.object,
+                                                                   color=color)
                     inventory.qty = qty
                     inventory.save()
-
-                else:
-                    print('DATA NOT FOUND')
-
-                print("##### FORM VALID")
-
-                print(F'HAS COLOR::: {"color" in inventory_form.cleaned_data}')
-
-
-                '''
-                !!! STILL NOT SAVING
-                m = inventory_form.save()
-                m.userpart = self.object
-                m.save()
-                '''
-
-                #inventory_form.save()
-                '''
-                model_instance = inventory_form.save(commit=False)
-                model_instance.post = self.object
-                model_instance.save()
-                '''
-
-                '''
-
-                THIS SHOULD BE ENOUGH FOR SAVING FORMS (DELETION MAYBE AS WELL???)
-                infentory_form.save()
-
-                WE SHOULDN"T NEED THIS FOR CREATION OR UPDATE, FORM.save() should do that
-
-                WHAT IF WE SAVE EMPTY FORMS???
-
-                inventory = Inventory.objects.create(
-                    userpart=self.object,
-                    color=inventory_form.cleaned_data['color'],
-                    qty=inventory_form.cleaned_data['qty']
-                    )
-                inventory.save()
-                '''
             else:
-                print("##### FORM INVALID")
-                #print(F"##### FORM INVALID::: {inventory_form}")
-                pass
-        # TODO - add "static 'formset/jquery.formset.js"
-        # Same as in Playground
+                form.add_error(None, 'Invalid Form')
+                return super().form_invalid(form)
 
         return super().form_valid(form)
-    '''
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs.update({'userpart': self.object})
-        return kwargs
-    '''
