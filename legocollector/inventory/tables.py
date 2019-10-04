f508d2f70ce8ed1596efe246dedcd6cbf595b565
eric <eric@home.com>
2019-10-04 21:48:31 +1000

    Add Image to table

--- legocollector/inventory/tables.py
+++ legocollector/inventory/tables.py
@@ -1,12 +1,21 @@
-from django_tables2 import CheckBoxColumn, LinkColumn, Table
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import format_html
+from django_tables2 import CheckBoxColumn, Column, LinkColumn, Table
 from django_tables2.utils import Accessor
 from django_tables2.views import SingleTableMixin  # (for import in views.py)  pylint: disable=unused-import
 
 from .models import Part, UserPart
 
 
+class PartImage(Column):
+    def render(self, value):
+        pic_url = static(F'inventory/PartColours/parts_-1/{value}.png')
+        return format_html(F'<img src="{pic_url}" alt="Part Picture" height="50" width="50">')
+
+
 class PartTable(Table):
     box_selection = CheckBoxColumn(accessor='id')
+    image = PartImage(accessor='part_num', verbose_name='Image')
 
     class Meta:  # pylint: disable=too-few-public-methods
         model = Part
