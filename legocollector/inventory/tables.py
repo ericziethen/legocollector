3b0fe9686e6ee68d9873ea82fd9b6ee7d6cbe016
ericziethen <48635802+ericziethen@users.noreply.github.com>
2019-10-06 19:11:34 +1100

    Feature/show part images (#47)
    * Exclude Static Color images from Repo
    
    * Sanple image
    
    * Add Image to table
    
    * Basic hoverig
    
    * Add simple zoom over hover
    
    * Add image zoom

--- legocollector/inventory/tables.py
+++ legocollector/inventory/tables.py
@@ -1,12 +1,24 @@
-from django_tables2 import CheckBoxColumn, LinkColumn, Table
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import format_html
+from django_tables2 import CheckBoxColumn, Column, LinkColumn, Table
 from django_tables2.utils import Accessor
 from django_tables2.views import SingleTableMixin  # (for import in views.py)  pylint: disable=unused-import
 
 from .models import Part, UserPart
 
 
+class PartImage(Column):
+    def render(self, value):
+        pic_url = static(F'inventory/PartColours/parts_-1/{value}.png')
+
+        img_class = 'part_image_zoom'
+
+        return format_html(F'<img src="{pic_url}" alt="Part Picture" class="{img_class}" height="50" width="50">')
+
+
 class PartTable(Table):
     box_selection = CheckBoxColumn(accessor='id')
+    image = PartImage(accessor='part_num', verbose_name='Image')
 
     class Meta:  # pylint: disable=too-few-public-methods
         model = Part
