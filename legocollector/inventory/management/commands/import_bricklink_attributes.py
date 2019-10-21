40889dc551ecfe95c9c9cda9791e45496631fd8e
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-10-21 13:31:07 +1100

    remove unneeded code

--- legocollector/inventory/management/commands/import_bricklink_attributes.py
+++ legocollector/inventory/management/commands/import_bricklink_attributes.py
@@ -40,34 +40,10 @@ class Command(BaseCommand):
                 item_x = item_tag.find('ITEMDIMX').text
                 item_y = item_tag.find('ITEMDIMY').text
                 item_z = item_tag.find('ITEMDIMZ').text
-                item_name = item_tag.find('ITEMNAME').text
 
                 if item_id:
                     if any([item_x, item_y, item_z]):
                         part = Part.objects.filter(part_num=item_id).first()
-
-
-                        !!! TODO
-                        When getting the part by name, multiple might have an identical name !!!
-                        Need to handle all
-                            -> Have separate function for each part
-
-                        !!! TODO
-                            - See if can get more relationships based on bricklink
-
-                            e.g. bricklink:
-                                    130	Baseplate, Road	6099px1	Baseplate, Road 32 x 32 9-Stud Landing Pad with Runway 'V' Pattern	100	32 x 32 x 0
-                                rebrickable
-                                    6099,Baseplate 32 x 32 Road 9-Stud Landing Pad,1,1
-                                    6099p01,Baseplate 32 x 32 Road 9-Stud Landing Pad with Runway Print,1,1
-                                    6099p02,Baseplate Road 32 x 32 9-Stud Landing Pad with Green Octagon Print [6988 / 6710],1,1
-                                    6099p03,Baseplate Road 32 x 32 9-Stud Landing Pad with Yellow Circle Print,1,1
-                                    6099p05,Baseplate 32 x 32 Road 9-Stud Landing Pad Type 2 (Orange),1,1
-                                    6099p06,Baseplate 32 x 32 Road 9-Stud Landing Pad Type 3 (Orange),1,1
-
-                        !!! We need a brickling id field in part, and look that one up, we can later populate it
-                        if not part:
-                            part = Part.objects.filter(name=item_name).first()
 
                         if part:
                             if item_x and item_y and (item_y > item_x):
@@ -101,7 +77,6 @@ class Command(BaseCommand):
         related_attributes_set_count = 0
         with transaction.atomic():
             for idx, part in enumerate(updated_parts_dic.values()):
-                #print('CHecking Part:', part.part_num)
                 for related_part in part.get_related_parts():
                     if related_part.part_num not in related_updated_dic:
                         related_part.width = part.width
