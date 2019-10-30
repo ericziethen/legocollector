d6c00a5bc33ca9f2fe5e3e2b1b122197834b3c12
eric <eric@home.com>
2019-10-30 21:03:45 +1100

    misc

--- legocollector/inventory/tests/forms/test_inventory_form.py
+++ legocollector/inventory/tests/forms/test_inventory_form.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from inventory.forms import InventoryForm
-from inventory.models import Part, PartCategory, UserPart
+from inventory.models import Color, Part, PartCategory, SetPart, UserPart
 
 
 class TestFormValidity(TestCase):
@@ -13,15 +13,23 @@ class TestFormValidity(TestCase):
 
         self.user = User.objects.create_user(  # nosec
             username='User', email='jacob@…', password='top_secret')
+
         self.part_category = PartCategory.objects.create(id=1, name='category')
+
         self.part = Part.objects.create(
             part_num='Part', name='A Part', category=self.part_category)
+
         self.user_part = UserPart.objects.create(user=self.user, part=self.part)
+
+        self.color_red = Color.objects.create(id='4', name='Red', rgb='C91A09')
+
+        self.set_part1 = SetPart.objects.create(
+            set_inventory=1, part=self.part, color=self.color_red, qty=1, is_spare=False)
 
     def run_form_test(self, expected_value, *, color=None, qty=None, removed=None, initial=None):
         form_data = {}
         if color:
-            form_data['color'] = 'Black'
+            form_data['color'] = self.color_red.pk
         if qty:
             form_data['qty'] = '15'
         if removed:
@@ -48,10 +56,9 @@ class TestFormValidity(TestCase):
     def test_form_only_initial_data(self):
         self.run_form_test(True, initial=True)
 
-    '''
     def test_form_only_color(self):
-        self.run_form_test({'color': '15'}, False)
-    '''
+        self.run_form_test(False, color=True)
+
 
 
 
