355433a6d128cd511e17697fe4fe95c7b1b5c3dd
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-10-31 10:36:50 +1100

    Simplify qty test

--- legocollector/inventory/tests/forms/test_inventory_form.py
+++ legocollector/inventory/tests/forms/test_inventory_form.py
@@ -29,25 +29,27 @@ class TestFormProcessing(TestCase):
         self.set_part2 = SetPart.objects.create(
             set_inventory=2, part=self.part, color=self.color_black, qty=1, is_spare=False)
 
-    def run_form(self, *, color=None, qty=None, removed=None, initial=None,
-                 new_color=None, initial_color=None, initial_qty=10):
+    def run_form(self, *, color=None, new_qty=None, removed=None, initial=None,
+                 new_color=None, initial_color=None, initial_qty=20):
         form_data = {}
         if color:
             if new_color:
                 form_data['color'] = new_color.pk
             else:
                 form_data['color'] = self.color_red.pk
-        if qty:
-            form_data['qty'] = '15'
+        if new_qty:
+            form_data['qty'] = new_qty
 
         form = InventoryForm(userpart=self.user_part, data=form_data)
-        form.full_clean()
 
         if initial:
             if initial_color:
                 form.initial_data = {'color': initial_color, 'qty': initial_qty}
             else:
                 form.initial_data = {'color': self.color_black, 'qty': initial_qty}
+
+        form.full_clean()
+
         if removed:
             form.cleaned_data['DELETE'] = True
 
@@ -56,15 +58,19 @@ class TestFormProcessing(TestCase):
     #########################################
     ###### Start of Form Validity Tests #####
     #########################################
-    def run_form_is_valid_test(self, expected_value, *, color=None, qty=None, removed=None, initial=None):
-        form = self.run_form(color=color, qty=qty, removed=removed, initial=initial)
+    def run_form_is_valid_test(self, expected_value, *, color=None, new_qty=None, removed=None, initial=None):
+        form = self.run_form(color=color, new_qty=new_qty, removed=removed, initial=initial)
+        
+        #print(F'\nCleaned Data: {form.cleaned_data}')
+        #print(F'  Initial Data: {form.initial_data}')
+        #print(F'  Changed Data: {form.changed_data}')
         if expected_value:
             self.assertTrue(form.is_valid(), str(form.errors) + F'\n\nCleaned Data: {form.cleaned_data}')
         else:
             self.assertFalse(form.is_valid(), str(form.errors) + F'\n\nCleaned Data: {form.cleaned_data}')
 
     def test_cleaned_data_populated(self):
-        form = self.run_form(color=True, qty=True, removed=True, initial=True)
+        form = self.run_form(color=True, new_qty=10, removed=True, initial=True)
 
         self.assertIn('color', form.cleaned_data)
         self.assertIn('qty', form.cleaned_data)
@@ -77,32 +83,33 @@ class TestFormProcessing(TestCase):
     def test_remove_form_always_valid(self):
         self.run_form_is_valid_test(True, removed=True)
         self.run_form_is_valid_test(True, removed=True, color=True)
-        self.run_form_is_valid_test(True, removed=True, color=True, qty=True)
+        self.run_form_is_valid_test(True, removed=True, color=True, new_qty=10)
         self.run_form_is_valid_test(True, removed=True, initial=True)
         self.run_form_is_valid_test(True, removed=True, initial=True, color=True)
-        self.run_form_is_valid_test(True, removed=True, initial=True, color=True, qty=True)
-        self.run_form_is_valid_test(True, removed=True, initial=True, qty=True)
-        self.run_form_is_valid_test(True, removed=True, qty=True)
+        self.run_form_is_valid_test(True, removed=True, initial=True, color=True, new_qty=10)
+        self.run_form_is_valid_test(True, removed=True, initial=True, new_qty=10)
+        self.run_form_is_valid_test(True, removed=True, new_qty=10)
 
     def test_form_values_removed_for_deletion(self):
         self.run_form_is_valid_test(True, initial=True)
 
     def test_form_newly_populated_form(self):
-        self.run_form_is_valid_test(True, color=True, qty=True)
+        self.run_form_is_valid_test(True, color=True, new_qty=10)
 
     def test_form_unchanged(self):
-        self.run_form_is_valid_test(True, initial=True, color=True, qty=True)
+        self.run_form_is_valid_test(True, initial=True, color=True, new_qty=10)
 
     def test_form_new_form_incomplete(self):
-        self.run_form_is_valid_test(False, qty=True)
+        self.run_form_is_valid_test(False, new_qty=10)
         self.run_form_is_valid_test(False, color=True)
 
     def test_form_incomplete_after_edit(self):
-        self.run_form_is_valid_test(False, initial=True, qty=True)
+        self.run_form_is_valid_test(False, initial=True, new_qty=10)
         self.run_form_is_valid_test(False, initial=True, color=True)
 
     ###########################################
     ###### Start of Form Processing Tests #####
     ###########################################
     
-    #print(F'Changed Data: {form.changed_data}')
+
+
