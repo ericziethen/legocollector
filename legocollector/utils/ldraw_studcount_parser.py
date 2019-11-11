ec41c308fac2f6247533411999f8ad803d265e71
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-11-12 09:01:09 +1100

    Move utility

---
+++ legocollector/utils/ldraw_studcount_parser.py
@@ -0,0 +1,239 @@
+import enum
+import json
+import operator
+import os
+
+from collections import defaultdict
+
+@enum.unique
+class LineType(enum.Enum):
+
+    # pylint: disable=invalid-name
+    UNKNOWN = 'Unknown'
+    COMMENT = 'Comments'
+    PART = 'Parts'
+    LINE = 'Lines'
+    TRIANGLE = 'Triangles'
+    QUAD = 'Quads'
+    OPTIONAL = 'Optional'
+
+@enum.unique
+class FileType(enum.Enum):
+
+    # pylint: disable=invalid-name
+    UNKNOWN = 'Unknown'
+    TOP_STUD = 'Top Stud'
+
+
+class FileListDic():
+
+    # TODO - We should only need the base dir and get folders to include ourselves,
+    # TODO - Then the key should be the hols relative path
+    # TODO - then remove from transform below
+    def __init__(self, parts_dir='parts', primitives_dir='p'):
+        self._files = {}
+
+        self._parse_dir(parts_dir)
+        self._parse_dir(primitives_dir)
+
+
+    def _parse_dir(self, full_dir):
+        for root, _, files in os.walk(full_dir):
+            for file_name in files:
+                rel_dir = os.path.relpath(root, start=full_dir)
+                rel_file = os.path.join(rel_dir, file_name).lstrip('.\\')  # We don't want the leading period
+
+                if rel_file in self:
+                    raise ValueError(F'Error: Cannot handle multiple Part Locations, Duplicate File: {file_name}')
+                self[rel_file] = os.path.join(full_dir, rel_file)
+
+    @staticmethod
+    def _keytransform(key) -> str:
+        # TODO - Check if we're fine with flattening the key, s\file.dat becomes file.dat
+        return str(key.lower())
+
+    def __setitem__(self, key, value: str) -> None:
+        self._files[self._keytransform(key)] = value
+
+    def __getitem__(self, key) -> str:
+        return self._files[self._keytransform(key)]
+
+    def __iter__(self):
+        return iter(self._files)
+
+    def __len__(self) -> int:
+        return len(self._files)
+
+    def __str__(self) -> str:
+        return str(self._files)
+
+    def __delitem__(self, key) -> None:
+        del self._files[self._keytransform(key)]
+
+
+class LdrawFile():
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.sup_part_files = []
+
+        self._parse(self.file_path)
+
+    def _parse(self, file_path):
+        with open(file_path, encoding='utf8', errors="surrogateescape") as fp:
+            for line in fp:
+                check_line = line.strip()
+                if line_type_from_line(check_line) == LineType.PART:
+                    self.sup_part_files.append(get_file_from_part_line(check_line))
+
+
+def line_type_from_line(line):
+    line_type = LineType.UNKNOWN
+
+    if line.startswith('0 '):
+        line_type = LineType.COMMENT
+    elif line.startswith('1 '):
+        line_type = LineType.PART
+    elif line.startswith('2 '):
+        line_type = LineType.LINE
+    elif line.startswith('3 '):
+        line_type = LineType.TRIANGLE
+    elif line.startswith('4 '):
+        line_type = LineType.QUAD
+    elif line.startswith('5 '):
+        line_type = LineType.OPTIONAL
+
+    return line_type
+
+
+def get_file_from_part_line(line):
+    return line.split()[-1]
+
+
+def get_ldraw_file_type(file_name):
+    file_type = FileType.UNKNOWN
+    top_stud_file_names = [
+        'stud.dat', 'studa.dat', 'studp01.dat', 'studel.dat', 'stud10.dat',
+        'stud15.dat', 'stud2.dat', 'stud2a.dat', 'stud17a.dat',
+        'stud9.dat', 'stud6.dat', 'stud6a.dat']
+
+    check_name = file_name.lower()
+    if check_name in top_stud_file_names:
+        file_type = FileType.TOP_STUD
+
+    return file_type
+
+
+def get_top_stud_count_for_file(file_path):
+    single_top_stud_file_types = [FileType.TOP_STUD]
+    if get_ldraw_file_type(os.path.basename(file_path)) in single_top_stud_file_types:
+        return 1
+    return 0
+
+
+# TODO - Count how often each file is being processed
+# TODO !!! Do we need a wrapper to process multiple parts, then we don't need to pass in dictionaries
+# TODO !!! Write a test that tests processed_files_dic
+def calc_stud_count_for_part_file(
+        file_path, file_dic, processed_files_dic=None, file_visited_count=None, rec_level=0):
+    if processed_files_dic is None:
+        processed_files_dic = {}
+
+    #print(F'{rec_level * "  "}  >> START Processing: {file_path} - {file_visited_count}')
+    count = get_top_stud_count_for_file(file_path)
+    if file_visited_count is not None:
+        if file_path not in file_visited_count:
+            file_visited_count[file_path.lower()] = 1
+            #print(F'{rec_level * "  "}   Setting File Visit Count to 1 for {file_path}')
+
+        else:
+            #print(F'{rec_level * "  "}   Increment File Visit Count from {file_visited_count[file_path.lower()]} for {file_path}')
+            file_visited_count[file_path.lower()] += 1
+
+    # Process sub files
+    if count == 0:
+        ldraw_file = LdrawFile(file_path)
+        for sub_file in ldraw_file.sup_part_files:
+            sub_file_path = file_dic[sub_file].lower()
+            #print(F'''{rec_level * "  "}    Checking Sub File: {sub_file_path} ({get_top_stud_count_for_file(sub_file)} - 
+            #            Visited? {sub_file_path in processed_files_dic} - {file_visited_count})''')
+
+            if sub_file_path in processed_files_dic:
+                count += processed_files_dic[sub_file_path]['top_stud_count']
+                #print(F'{rec_level * "  "}      Existing Count: {count}')
+            else:
+                sub_file_count = calc_stud_count_for_part_file(
+                    sub_file_path, file_dic, processed_files_dic,
+                    file_visited_count, rec_level + 1)
+                count += sub_file_count
+                #print(F'{rec_level * "  "}      Calculated Count: {count}')
+
+                processed_files_dic[sub_file_path] = {'top_stud_count': sub_file_count}
+                #print(F'{rec_level * "  "}    Sub Count Set for {sub_file}')
+
+    if file_path not in processed_files_dic:
+        processed_files_dic[file_path] = {'top_stud_count': count}
+    return count
+
+
+def create_json_for_parts(json_out_file_path):
+    prim_dir = R'D:\Downloads\Finished\# Lego\ldraw\complete_2019.11.05\ldraw\p'
+    parts_dir = R'D:\Downloads\Finished\# Lego\ldraw\complete_2019.11.05\ldraw\parts'
+
+    file_dic = FileListDic(parts_dir=parts_dir, primitives_dir=prim_dir)
+    processed_files_dic = {}
+    parts_dic = {}
+
+    for idx, file_name in enumerate(os.listdir(parts_dir), 1):
+        file_path = os.path.join(parts_dir, file_name)
+        part_num = os.path.splitext(file_name)[0]
+
+        if not os.path.isfile(file_path) or not file_name.lower().endswith('.dat'):
+            continue
+
+        stud_count = calc_stud_count_for_part_file(file_path, file_dic, processed_files_dic)
+        parts_dic[part_num] = {}
+        parts_dic[part_num]['stud_count'] = stud_count
+
+        if idx % 500 == 0:
+            print(F'Processed {idx} parts')
+
+    with open(json_out_file_path, 'w', encoding='utf-8') as file_ptr:
+        json.dump(parts_dic, file_ptr)
+
+
+
+def main():
+    prim_dir = R'.eric\ldraw\complete\ldraw\p'
+    parts_dir = R'.eric\ldraw\complete\ldraw\parts'
+
+    file_dic = FileListDic(parts_dir=parts_dir, primitives_dir=prim_dir)
+    processed_files_dic = {}
+    stud_counts = {}
+
+    #for idx, file_name in enumerate(glob.glob(parts_dir + R'\*.dat')):
+    for idx, file_name in enumerate(os.listdir(parts_dir)):
+        
+        #print(F'CHECK: {file_name}')
+        file_path = os.path.join(parts_dir, file_name)
+        if not os.path.isfile(file_path) or not file_name.lower().endswith('.dat'):
+            #print(F'  >> SKIP')
+            continue
+        #if idx > 5600:
+        #    print(F'{file_name}')
+        #print(F'{file_name}')
+        stud_count = calc_stud_count_for_part_file(file_path, file_dic, processed_files_dic)
+        stud_counts[file_name] = stud_count
+
+        if idx % 100 == 0:
+            print(F'Processed Files: {idx}')
+
+    sorted_stud_counts = sorted(stud_counts.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_stud_counts_str = '\n'.join([F'{tup[1]}:: {tup[0]}' for tup in sorted_stud_counts if tup[1] > 0])
+    print(F'STUD COUNTS: {sorted_stud_counts_str}')
+    #print(F'TOTAL FILE VISITS: {sum(ERIC_FILE_VISIT_COUNT.values())}\n\n')
+
+
+if __name__ == '__main__':
+    #main()
+    create_json_for_parts('ldraw_studcount.json')
