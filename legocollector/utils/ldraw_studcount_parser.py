ca03112c2aeacbe8afce5600888af63395c65bf4
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-11-13 08:53:29 +1100

    misc

--- legocollector/utils/ldraw_studcount_parser.py
+++ legocollector/utils/ldraw_studcount_parser.py
@@ -2,6 +2,8 @@ import enum
 import json
 import os
 
+from pathlib Path
+
 
 @enum.unique
 class LineType(enum.Enum):
@@ -46,12 +48,16 @@ class FileListDic():
             for file_name in files:
                 print(F'    file_name: {file_name}')
                 rel_dir = os.path.relpath(root, start=full_dir)
-                file_path = os.path.join(rel_dir, file_name).lstrip('.\\').lstrip('/')  # We don't want the leading period
-                rel_file = os.path.normpath(file_path)
+                
+                #file_path = os.path.join(rel_dir, file_name).lstrip('.\\').lstrip('/')  # We don't want the leading period
+                #rel_file = os.path.normpath(file_path)
+
+                rel_file = Path(rel_dir)
 
                 if rel_file in self:
                     raise ValueError(F'Error: Cannot handle multiple Part Locations, Duplicate File: {file_name}')
-                self[rel_file] = os.path.normpath(os.path.join(full_dir, rel_file))
+                #self[rel_file] = os.path.normpath(os.path.join(full_dir, rel_file))
+                self[rel_file] = Path(full_dir) / rel_file
         print(F'  RESULT: _parse_dir({self._files})')
 
     @staticmethod
