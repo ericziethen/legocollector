6884a2f3b6975be2f99f024279cd050db1f221ad
ericziethen <48635802+ericziethen@users.noreply.github.com>
2019-11-14 10:24:31 +1100

    Feature/include studs in related attributes (#114)
    * Initial Stud count setting for related parts
    
    * CLean up comments
    
    * Fix linting issue

--- legocollector/inventory/management/commands/set_related_attributes.py
+++ legocollector/inventory/management/commands/set_related_attributes.py
@@ -14,22 +14,53 @@ class Command(BaseCommand):
         self.print_attribute_details()
 
         related_attributes_set_count = 0
+        related_stud_counts_set = 0
         with transaction.atomic():
             for idx, part in enumerate(Part.objects.all()):
                 if part.part_num not in processed_parts:
                     related_parts = part.get_related_parts(parents=True, children=True, transitive=True)
 
                     part_family = sorted(related_parts + [part], key=lambda p: p.attribute_count, reverse=True)
-                    highest_count_part = part_family[0]  # Will always exist since we added ourselves
 
+                    # For Processing Stud Counts we need to be carefull as related parts may have a different stud count
+                    # e.g. the following 2 related parts have a different stud count
+                    #       https://rebrickable.com/parts/10a/baseplate-24-x-32-with-squared-corners/
+                    #       https://rebrickable.com/parts/10b/baseplate-24-x-32-with-rounded-corners/
+
+                    # Figure out the Stud Count, only take the count if there are not more than 1 different stud counts
+                    stud_count = None
                     for related_part in part_family:
+                        if related_part.stud_count is not None:
+                            if stud_count and stud_count != related_part.stud_count:
+                                # a different stud count found, igore whole family
+                                stud_count = None
+                                break
+
+                            if stud_count is None:
+                                stud_count = related_part.stud_count
+
+                    highest_count_part = part_family[0]  # Will always exist since we added ourselves
+                    for related_part in part_family:
+                        update = False
+
+                        # Set the Attributes
                         if ((highest_count_part is not related_part) and
                                 (highest_count_part.attribute_count > related_part.attribute_count)):
+                            update = True
                             related_part.width = highest_count_part.width
                             related_part.length = highest_count_part.length
                             related_part.height = highest_count_part.height
-                            related_part.save()
                             related_attributes_set_count += 1
+
+                        # Set the Stud Count
+                        if stud_count is not None:
+                            update = True
+                            related_part.stud_count = stud_count
+                            related_stud_counts_set += 1
+
+                        if update:
+                            related_part.save()
+
                         processed_parts[related_part.part_num] = True
 
                         if related_attributes_set_count and (related_attributes_set_count % 1000) == 0:
@@ -39,6 +70,7 @@ class Command(BaseCommand):
                     self.stdout.write(F'  {idx} Parts Processed')
 
         self.stdout.write(F'  Attributes Set on: {related_attributes_set_count} related parts')
+        self.stdout.write(F'  Stud Count set on: {related_stud_counts_set} related parts')
         self.print_attribute_details()
 
     def print_attribute_details(self):
