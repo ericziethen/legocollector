c47667ea7edb92227b779fcf0acb4d5025224524
ericziethen <48635802+ericziethen@users.noreply.github.com>
2019-11-21 22:48:02 +1100

    Feature/run pytest for all tests (#125)
    * Make requirements file utf8
    
    * Add pytest-django
    
    * Move generic tests into legocollector app
    
    * only run pytests and format coverage output
    
    * Generate html report as well
    
    * Fix Bandt script
    
    * Ignore test path in bandit

--- dev/Linting/RunBandit.sh
+++ dev/Linting/RunBandit.sh
@@ -3,13 +3,15 @@
 MODULE_NAME=legocollector
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_MAIN_DIR=$SCRIPT_PATH/../..
-MODULE_PATH=$PROJ_MAIN_DIR/$MODULE_NAME
+MODULE_PATH=$MODULE_NAME
+
+pushd "$PROJ_MAIN_DIR"
 
 echo SCRIPT_PATH: $SCRIPT_PATH
 echo PROJ_MAIN_DIR: $PROJ_MAIN_DIR
 echo MODULE_PATH: $MODULE_PATH
 
-bandit -r "$MODULE_PATH"
+bandit -r "$MODULE_PATH" --exclude "legocollector/tests/"
 return_code=$?
 
 if [[ $return_code -eq  0 ]];
@@ -18,5 +20,7 @@ then
 else
     echo "*** Some Issues Found"
 fi
+
+popd
 
 exit $return_code
